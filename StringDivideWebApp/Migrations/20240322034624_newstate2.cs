0df7e33fd28ec16fb05c277b33f91777a9624e4d
NachiketGavad <nachiketgavad@gmail.com>
2024-03-23 00:36:51 +0530

    index update

---
+++ StringDivideWebApp/Migrations/20240322034624_newstate2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StringDivideWebApp.Migrations
+{
+    public partial class newstate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ StringDivideWebApp/Migrations/20240322190411_newstate3.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StringDivideWebApp.Migrations
+{
+    public partial class newstate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
