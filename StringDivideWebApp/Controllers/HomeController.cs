f8af050b21ba7164cf645edae401802cb94ed996
NachiketGavad <nachiketgavad@gmail.com>
2024-03-26 18:37:23 +0530

    Added Calculator to website, Using ajax for handling requests without page reload,

--- StringDivideWebApp/Controllers/HomeController.cs
+++ StringDivideWebApp/Controllers/HomeController.cs
@@ -147,5 +147,37 @@ namespace StringDivideWebApp.Controllers
 
             return result;
         }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+            switch (operation)
+            {
+                case "add":
+                    result = num1 + num2;
+                    break;
+                case "subtract":
+                    result = num1 - num2;
+                    break;
+                case "multiply":
+                    result = num1 * num2;
+                    break;
+                case "divide":
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        return BadRequest("Cannot divide by zero");
+                    break;
+                default:
+                    return BadRequest("Invalid operation");
+            }
+            return Ok(result);
+        }
     }
 }
